8ed2c2522fa34592dc6036d7828ed17411df3aca
Anderson  Santana <andersonsantana.ti@outlook.com>
2021-01-14 17:29:36 -0300

    Exibição da lista de eventos na View

--- src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventoController {
@@ -21,5 +22,12 @@ public class EventoController {
     public String form (Evento evento){
         eventoRepository.save(evento);
         return"redirect:/cadastrarEvento";
+    }
+    @RequestMapping(value = "/eventos")
+    public ModelAndView listaEventos(){
+        ModelAndView modelAndView = new ModelAndView("index");
+        Iterable<Evento> eventos = eventoRepository.findAll();
+        modelAndView.addObject("eventos", eventos);
+        return modelAndView;
     }
 }
